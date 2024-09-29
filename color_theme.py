9c3a7e86e0490322e2942811790b51771849786b
bruno <cardoso.bc@gmail.com>
2024-09-29 14:37:27 -0300

    color_theme: fix parameters names

--- color_theme.py
+++ color_theme.py
@@ -18,18 +18,18 @@ theme_name = "ocean"
 theme_path = None
 color_palette = None
 
-def get_theme_path (theme_name:str):
-    return os.path.join(theme_dir, theme_name.removesuffix(".yaml") + ".yaml")
+def get_theme_path (name:str):
+    return os.path.join(theme_dir, name.removesuffix(".yaml") + ".yaml")
 
-def read_theme (theme_path:str):
-    with open(theme_path, 'r') as f:
+def read_theme (path:str):
+    with open(path, 'r') as f:
         palette_data = yaml.safe_load(f)
     return ["#" + color for color in list(palette_data.values())[2:]]
 
-def set_theme (theme_name:str):
+def set_theme (name:str):
     global theme_name, theme_path, color_palette
-    theme_name = theme_name.removesuffix(".yaml")
-    theme_path = get_theme_path(theme_name)
+    theme_name = name.removesuffix(".yaml")
+    theme_path = get_theme_path(name)
     color_palette = read_theme(theme_path)
 
 def list_themes_yaml ():
@@ -39,11 +39,15 @@ def list_themes ():
     [print(theme.removesuffix(".yaml")) for theme in list_themes_yaml()]
     print("\nCurrent:", theme_name)
 
-def list_colors (theme_name=None):
-    if theme_name:
-        return read_theme(get_theme_path(theme_name))
+def list_colors (name:str|None=None):
+    if name:
+        return read_theme(get_theme_path(name or theme_name))
     else:
         return color_palette
+
+def print_colors (name:str|None=None):
+    for c in enumerate(list_colors(name or theme_name)):
+        print("%2s: %s" % (c[0], c[1]))
 
 def pick_color (n=1, any=False, range_a=None, range_b=None):
     if any or isinstance(range_a, int) or isinstance(range_b, int):
