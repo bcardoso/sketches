3dd0ba7298d43e74bb92a822db6c5dd632b82a7d
bruno <cardoso.bc@gmail.com>
2024-09-29 22:42:54 -0300

    tiles: tile_grid, tile_arc, tile_line

--- tiles.py
+++ tiles.py
@@ -2,6 +2,11 @@ import py5
 
 tile_size = 50
 
+def tile_grid (width, height, size):
+    for x in range(0, width, size):
+        for y in range(0, height, size):
+            yield (x, y)
+
 def tile_square (x, y, size, fill):
     py5.fill(fill)
     py5.stroke(fill)
@@ -22,3 +27,25 @@ def tile_triangle (x, y, size, fill, r=0):
     else:
         # ◺ lower left
         py5.triangle(x,y, x,y+size, x+size,y+size)
+
+def tile_arc (x, y, size, color, weight=3, r=0):
+    py5.no_fill()
+    py5.stroke(color)
+    py5.stroke_weight(weight)
+    if r == 0:
+        py5.arc(x,y, size,size, 0, py5.PI/2)
+        py5.arc(x+size, y+size, size,size, py5.PI, py5.PI+py5.PI/2)
+    else:
+        py5.arc(x+size, y, size,size, py5.PI/2,py5.PI)
+        py5.arc(x, y+size, size,size, py5.PI+py5.PI/2,2*py5.PI)
+
+def tile_line (x, y, size, color, weight=3, r=0):
+    py5.no_fill()
+    py5.stroke(color)
+    py5.stroke_weight(weight)
+    if r == 0:
+        py5.line(x,y+size/2, x+size/2,y)
+        py5.line(x+size/2,y+size, x+size,y+size/2)
+    else:
+        py5.line(x,y+size/2, x+size/2,y+size)
+        py5.line(x+size/2,y, x+size,y+size/2)
