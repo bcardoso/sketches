b2691eb6a57b53c1d0890b0e0040f2e97754baed
bruno <cardoso.bc@gmail.com>
2025-11-02 16:32:26 -0300

    refactor(Series): type annotations

--- series.py
+++ series.py
@@ -1,32 +1,43 @@
 import random
+from collections.abc import Iterator
 
 # * Series
 
 # ** Fibonacci
 
 # Fibonacci numbers: https://oeis.org/A000045
-# Lucas numbers: https://oeis.org/A000032
 
-def fibonacci (n=30, a=0, b=1):
+
+def fibonacci(limit: int = 30, a: int = 0, b: int = 1) -> list[int]:
+    """Return the Fibonacci numbers until limit."""
     numbers = [a] if a > 0 else []
-    while len(numbers) < n:
+    while len(numbers) < limit:
         a, b = b, a + b
         numbers.append(a)
     return numbers
 
-def lucas (n=30):
-    return fibonacci(n, 2, 1)
+
+# ** Lucas
+
+# Lucas numbers: https://oeis.org/A000032
+
+
+def lucas(limit: int = 30) -> list[int]:
+    """Return the Lucas numbers until limit."""
+    return fibonacci(limit, 2, 1)
 
 
 # ** Sieve of Eratosthenes
 
-def primes (n=30):
+
+def primes(limit: int = 30) -> list[int]:
+    """Return the list of the first prime numbers until limit."""
     numbers = []
-    for n in range(2, n):
+    for n in range(2, limit):
         for factor in numbers:
             if n % factor == 0:
                 break
-        else: # no break
+        else:  # no break
             numbers.append(n)
     return numbers
 
@@ -35,7 +46,16 @@ def primes (n=30):
 
 # ** Binary sequence
 
-def binary_seq (bits=9, shuffle=False):
+
+def binary_seq(bits: int = 9, shuffle: bool = False) -> Iterator[str]:
+    """Return zero-padded binary strings for the number of bits.
+
+    If shuffle is True, shuffle the sequence.
+
+    Yields:
+        str: A zero-padded binary string.
+
+    """
     seq = list(range(2**bits))
     if shuffle:
         random.shuffle(seq)
