34acdee1d05d2397e68d18978efee252b93f9d96
bruno <cardoso.bc@gmail.com>
2025-11-02 16:33:11 -0300

    refactor(tiles): Tile class

--- tiles.py
+++ tiles.py
@@ -1,71 +1,115 @@
-import py5
 import math
+from dataclasses import dataclass
+from textwrap import fill
 
-def tile_grid (width, height, size):
-    for x in range(0, width, size):
-        for y in range(0, height, size):
+import py5
+
+
+def tile_grid(width: int, height: int, tile_size: int) -> tuple[int, int]:
+    """Return the points of a tile grid.
+
+    Yields:
+        tuple[int, int]: The point in the tile grid.
+
+    """
+    for x in range(0, width, tile_size):
+        for y in range(0, height, tile_size):
             yield (x, y)
 
-def tile_square (x, y, size, fill):
-    py5.fill(fill)
-    py5.stroke(fill)
-    py5.rect(x, y, size, size)
 
-def tile_square_depth (x, y, size, f0, f1, scale=10, depth=7):
-    for d in range(depth+1):
-        tile_square(x+(d*scale), y+(d*scale),
-                    size-(2*d*scale),
-                    f0 if d%2==0 else f1)
+@dataclass
+class Tile:
+    """A tile."""
 
-def tile_triangle (x, y, size, fill, r=0):
-    py5.fill(fill)
-    py5.stroke(fill)
-    if r == 0:
-        # ◸ upper left
-        py5.triangle(x,y, x,y+size, x+size,y)
-    elif r == 1:
-        # ◹ upper right
-        py5.triangle(x,y, x+size,y, x+size,y+size)
-    elif r == 2:
-        # ◿ lower right
-        py5.triangle(x,y+size, x+size,y, x+size,y+size)
-    else:
-        # ◺ lower left
-        py5.triangle(x,y, x,y+size, x+size,y+size)
+    x: float
+    y: float
+    size: float
+    fill_color: str | None = None
 
-def tile_arc (x, y, size, color, weight=3, r=0):
-    py5.no_fill()
-    py5.stroke(color)
-    py5.stroke_weight(weight)
-    if r == 0:
-        py5.arc(x,y, size,size, 0, py5.PI/2)
-        py5.arc(x+size, y+size, size,size, py5.PI, py5.PI+py5.PI/2)
-    else:
-        py5.arc(x+size, y, size,size, py5.PI/2,py5.PI)
-        py5.arc(x, y+size, size,size, py5.PI+py5.PI/2,2*py5.PI)
+    def square(self, color: str | None = None) -> None:
+        """Draw squared tile."""
+        py5.fill(color or self.fill_color)
+        py5.stroke(color or self.fill_color)
+        py5.rect(self.x, self.y, self.size, self.size)
 
-def tile_line (x, y, size, color, weight=3, r=0):
-    py5.no_fill()
-    py5.stroke(color)
-    py5.stroke_weight(weight)
-    if r == 0:
-        py5.line(x,y+size/2, x+size/2,y)
-        py5.line(x+size/2,y+size, x+size,y+size/2)
-    else:
-        py5.line(x,y+size/2, x+size/2,y+size)
-        py5.line(x+size/2,y, x+size,y+size/2)
+    def layered_squares(
+        self,
+        fill_color1: str,
+        fill_color2: str,
+        scale: float = 10.0,
+        layers: int = 7,
+    ) -> None:
+        """Draw layered squares within tile."""
+        for d in range(layers + 1):
+            self.square(
+                self.x + (d * scale),
+                self.y + (d * scale),
+                self.size - (2 * d * scale),
+                fill_color1 if d % 2 == 0 else fill_color2,
+            )
 
-def tile_bits (x, y, size, bits, c0, c1, cs=None):
-    part = math.floor(math.sqrt(len(bits)))
-    size = size//part
-    pos = 0
-    for i in range(part):
-        for j in range(part):
-            if bits[-1*pos]=='1':
-                c = c1
-            else:
-                c = c0
-            py5.fill(c)
-            py5.stroke(cs or c)
-            py5.rect(x + i*size, y + j*size, size, size)
-            pos += 1
+    def triangle(self, color: str, r: int = 0) -> None:
+        """Draw a triangle within tile."""
+        py5.fill(color)
+        py5.stroke(color)
+        x, y, size = self.x, self.y, self.size
+        if r == 0:
+            # ◸ upper left
+            py5.triangle(x, y, x, y + size, x + size, y)
+        elif r == 1:
+            # ◹ upper right
+            py5.triangle(x, y, x + size, y, x + size, y + size)
+        elif r == 2:
+            # ◿ lower right
+            py5.triangle(x, y + size, x + size, y, x + size, y + size)
+        else:
+            # ◺ lower left
+            py5.triangle(x, y, x, y + size, x + size, y + size)
+
+    def arcs(self, color: str, weight: float = 3.0, r: int = 0) -> None:
+        """Draw composable arcs within tile."""
+        py5.no_fill()
+        py5.stroke(color)
+        py5.stroke_weight(weight)
+        x, y, size = self.x, self.y, self.size
+        if r == 0:
+            py5.arc(x, y, size, size, 0, py5.PI / 2)
+            py5.arc(
+                x + size, y + size, size, size, py5.PI, py5.PI + py5.PI / 2
+            )
+        else:
+            py5.arc(x + size, y, size, size, py5.PI / 2, py5.PI)
+            py5.arc(x, y + size, size, size, py5.PI + py5.PI / 2, 2 * py5.PI)
+
+    def lines(self, color: str, weight: float = 3.0, r: int = 0) -> None:
+        """Draw composable lines within tile."""
+        py5.no_fill()
+        py5.stroke(color)
+        py5.stroke_weight(weight)
+        x, y, size = self.x, self.y, self.size
+        if r == 0:
+            py5.line(x, y + size / 2, x + size / 2, y)
+            py5.line(x + size / 2, y + size, x + size, y + size / 2)
+        else:
+            py5.line(x, y + size / 2, x + size / 2, y + size)
+            py5.line(x + size / 2, y, x + size, y + size / 2)
+
+    def bits(
+        self,
+        binary: str,
+        color0: str,
+        color1: str,
+        stroke_color: str | None = None,
+    ) -> None:
+        """Draw squared binary bits within tile."""
+        x, y, size = self.x, self.y, self.size
+        part = math.floor(math.sqrt(len(binary)))
+        size //= part
+        pos = 0
+        for i in range(part):
+            for j in range(part):
+                c = color1 if binary[-1 * pos] == "1" else color0
+                py5.fill(c)
+                py5.stroke(stroke_color or c)
+                py5.rect(x + i * size, y + j * size, size, size)
+                pos += 1
